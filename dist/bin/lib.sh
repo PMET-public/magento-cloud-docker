177854c1e503f3159e6d48fee301634720e9c440
Keith Bentrup <keith@keithbentrup.com>
2020-04-13 09:33:04 -0400

    updates

--- dist/bin/lib.sh
+++ dist/bin/lib.sh
@@ -8,6 +8,12 @@ if [[ ! -z "$debug" ]]; then
   set -x
 fi
 
+###
+#
+# messaging functions
+#
+###
+
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[1;33m'
@@ -25,9 +31,14 @@ msg() {
   printf "\n${green}${@}${no_color}\n\n"
 }
 
+###
+#
+# utility functions
+#
+###
 
-get_project_name() {
-  perl -ne 's/.*VIRTUAL_HOST=([^.]*).*/\1/ and print' "$(get_lib_dir)/../docker-compose.yml"
+export_compose_project_name() {
+  export COMPOSE_PROJECT_NAME=$(perl -ne 's/.*VIRTUAL_HOST=([^.]*).*/\1/ and print' "$(get_lib_dir)/../docker-compose.yml")
 }
 
 get_host() {
@@ -36,4 +47,42 @@ get_host() {
 
 get_lib_dir() {
   echo "$(dirname $(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "$BASH_SOURCE"))"
+}
+
+get_log_file() {
+  echo "$(get_lib_dir)/../../$COMPOSE_PROJECT_NAME.log"
+}
+
+timestamp_msg() {
+  echo "[$(date -u +%FT%TZ)] $(msg ${@})"
+}
+
+###
+#
+# update  functions
+#
+###
+
+app_repo="https://raw.githubusercontent.com/PMET-public/magento-cloud-docker/master/dist/bin"
+app_files=(lib.sh manage-dockerized-cloud-env.sh dockerize-cloud-env.sh)
+master_dir="$(get_lib_dir)/master"
+
+download_latest_master() {
+  mkdir -p "$master_dir"
+  cd "$master_dir"
+  curl_list="$(IFS=,; echo "${app_files[*]}")"
+  curl -v -O "$app_repo/{$curl_list}" 2>&1 | grep '< HTTP/1.1 ' | grep -q -v 200 || \
+    rm ${app_files[*]} || : # only keep if all downloads return 200
+}
+download_latest_master
+exit
+
+check_for_updates() {
+  for i in "${app_files[@]}"; do
+    diff "$master_dir/$i" "$(get_lib_dir)/$i" || return $?
+  done
+}
+
+update_from_master() {
+  mv "$master_dir/*" "$(get_lib_dir)/"
 }
--- dist/bin/manage-dockerized-cloud-env.sh
+++ dist/bin/manage-dockerized-cloud-env.sh
@@ -8,19 +8,31 @@ source "$(dirname $(python -c "import os; import sys; print(os.path.realpath(sys
 
 # cd to app dir containing relevant docker-compose files
 cd $(get_lib_dir)/..
-export COMPOSE_PROJECT_NAME=$(get_project_name)
+export_compose_project_name
 
 has_no_args() {
   [[ ${#BASH_ARGV[@]} -eq 0 ]]
   return $?
 }
 
-echo_in_terminal() {
+do_in_terminal() {
   osascript -e "tell app \"Terminal\"
     if not (exists window 1) then reopen
     activate
-    do script \"echo $1\" in window 1
+    do script \"$1\" in window 1
   end tell" 2>&1 >> /tmp/out
+}
+
+write_to_bash_script() {
+  local tmp_script_dir="$(get_lib_dir)/../../tmp"
+  mkdir -p "$tmp_script_dir"
+  local script="$tmp_script_dir/$(date -u "+%Y%m%d_%H%M%SZ").sh"
+  echo "#!/bin/bash
+source \"$(get_lib_dir)/lib.sh\"
+${@}
+" > "$script"
+  chmod +x "$script"
+  echo "$script"
 }
 
 is_app_installed() {
@@ -33,162 +45,235 @@ is_app_installed() {
 }
 
 is_app_running() {
-  [[ ! -z "$app_is_installed" ]] ||
+  [[ ! -z "$app_is_running" ]] ||
     {
-      docker ps -a | grep -q " ${COMPOSE_PROJECT_NAME}_build_1"
-      app_is_installed=$?
+      docker ps | grep -q " ${COMPOSE_PROJECT_NAME}_db_1"
+      app_is_running=$?
     }
-  return "$app_is_installed"
+  return "$app_is_running"
 }
 
-# menu item functions
 
-install_app() {
-  is_app_installed && return
-  local menu_item_text="Install & open app in browser"
+###
+#
+# menu item functions
+#
+###
+
+update_this_management_app() {
+  # menu logic
+  #! is_update_available && return # menu item n/a if not installed
+  local menu_item_text="Update this managing app"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
 
-  # create containers but do not start
-  docker-compose up --no-start
-  # copy db files to db container
-  docker cp .docker/mysql/docker-entrypoint-initdb.d ${COMPOSE_PROJECT_NAME}_db_1:/
-  # copy over app files to build container
-  tar -cf - --exclude .docker --exclude .composer.tar.gz . | docker cp - ${COMPOSE_PROJECT_NAME}_build_1:/app
-  tar -zxf .composer.tar.gz | docker cp - ${COMPOSE_PROJECT_NAME}_build_1:/app
-  docker cp app/etc ${COMPOSE_PROJECT_NAME}_deploy_1:/app/app/
-  tar -zxf media.tar.gz | docker cp - ${COMPOSE_PROJECT_NAME}_build_1:/app || :
-  docker-compose up -d db build
-  docker-compose run build cloud-build
-  docker-compose up -d
-  docker-compose run deploy cloud-deploy
-  docker-compose run deploy magento-command config:set system/full_page_cache/caching_application 2 --lock-env
-  docker-compose run deploy cloud-post-deploy
-  open "http://$(get_host)"
+  # function logic
+  do_in_terminal "echo $BASH_ARGV"
+  exit
+}
+
+install_app() {
+  # menu logic
+  is_app_installed && return
+  local menu_item_text="Install & open Magento app in browser"
+  has_no_args &&
+    echo "$menu_item_text" && return
+  [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
+
+  # function logic
+  {
+    timestamp_msg "$menu_item_text"
+    # create containers but do not start
+    docker-compose up --no-start
+    # copy db files to db container
+    docker cp .docker/mysql/docker-entrypoint-initdb.d ${COMPOSE_PROJECT_NAME}_db_1:/
+    # copy over app files to build container
+    tar -cf - --exclude .docker --exclude .composer.tar.gz . | docker cp - ${COMPOSE_PROJECT_NAME}_build_1:/app
+    tar -zxf .composer.tar.gz | docker cp - ${COMPOSE_PROJECT_NAME}_build_1:/app
+    docker cp app/etc ${COMPOSE_PROJECT_NAME}_deploy_1:/app/app/
+    tar -zxf media.tar.gz | docker cp - ${COMPOSE_PROJECT_NAME}_build_1:/app || :
+    docker-compose up -d db build
+    docker-compose run build cloud-build
+    docker-compose up -d
+    docker-compose run deploy cloud-deploy
+    docker-compose run deploy magento-command config:set system/full_page_cache/caching_application 2 --lock-env
+    docker-compose run deploy cloud-post-deploy
+    open "http://$(get_host)"
+  } >> "$(get_log_file)" 2>&1
   exit
 }
 
 open_app() {
+  # menu logic
   ! is_app_installed && return # menu item n/a if not installed
-  local menu_item_text="Open app in browser"
+  local menu_item_text="Open Magento app in browser"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
 
+  # function logic
   open "http://$(get_host)"
   exit
 }
 
 stop_app() {
+  # menu logic
   ! is_app_installed && return # menu item n/a if not installed
   ! is_app_running && return # menu item n/a if not running
-  local menu_item_text="Stop app"
+  local menu_item_text="Stop Magento app"
   has_no_args && echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+
+  # function logic
+  {
+    timestamp_msg "$menu_item_text"
+    docker-compose stop
+  } >> "$(get_log_file)" 2>&1
   exit
 }
 
-resume_app() {
+restart_app() {
+  # menu logic
   ! is_app_installed && return # menu item n/a if not installed
   is_app_running && return # menu item n/a if running
-  local menu_item_text="Resume app"
+  local menu_item_text="Restart Magento app"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
-  exit
-}
 
-reset_app() {
-  ! is_app_installed && return # menu item n/a if not installed
-  local menu_item_text="Reset app to original state"
-  has_no_args &&
-    echo "$menu_item_text" && return
-  [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+  # function logic
+  {
+    timestamp_msg "$menu_item_text"
+    docker-compose start
+  } >> "$(get_log_file)" 2>&1
   exit
 }
 
 sync_app_to_remote() {
-  local menu_item_text="Sync app to remote env"
+  # menu logic
+  ! is_app_installed && return # menu item n/a if not installed
+  local menu_item_text="TODO - Sync Magento app to remote env"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+
+  # function logic
+  do_in_terminal "echo $BASH_ARGV"
   exit
 }
 
 clone_app() {
-  local menu_item_text="Clone to new app"
+  # menu logic
+  local menu_item_text="TODO - Clone to new Magento app"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+
+  # function logic
+  do_in_terminal "echo $BASH_ARGV"
   exit
 }
 
-start_shell() {
+start_shell_in_app() {
+  # menu logic
   ! is_app_installed && return # menu item n/a if not installed
-  local menu_item_text="Start shell in app"
+  local menu_item_text="Start shell in Magento app"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+
+  # function logic
+  do_in_terminal "echo $BASH_ARGV"
+  exit
+}
+
+start_management_shell() {
+  # menu logic
+  local menu_item_text="Start management app shell"
+  has_no_args &&
+    echo "$menu_item_text" && return
+  [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
+
+  # function logic
+  do_in_terminal "cd $(get_lib_dir)/..; docker-compose run deploy bash"
   exit
 }
 
 show_app_logs() {
+  # menu logic
   ! is_app_installed && return # menu item n/a if not installed
-  local menu_item_text="Show app logs"
+  local menu_item_text="Show Magento app logs"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+
+  # function logic
+  do_in_terminal "echo $BASH_ARGV"
+  exit
+}
+
+show_management_app_log() {
+  # menu logic
+  [[ ! -f "$(get_log_file)" ]] && return # menu item n/a if no log
+  local menu_item_text="Show this managing app's log"
+  has_no_args &&
+    echo "$menu_item_text" && return
+  [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
+
+  # function logic
+  local script="$(write_to_bash_script "
+  msg Last 20 + follow
+  tail -n 20 -f $(get_log_file)
+  ")"
+  do_in_terminal "$script"
   exit
 }
 
 uninstall_app() {
+  # menu logic
   ! is_app_installed && return # menu item n/a if not installed
-  local menu_item_text="Uninstall this app"
+  local menu_item_text="Uninstall this Magento app"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
-  exit
-}
 
-uninstall_other_apps() {
-  local menu_item_text="Uninstall all other Magento apps"
-  has_no_args &&
-    echo "$menu_item_text" && return
-  [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+  # function logic
+  {
+    timestamp_msg "$menu_item_text"
+    docker-compose down -v
+  } >> "$(get_log_file)" 2>&1
   exit
 }
 
 stop_other_apps() {
+  # menu logic
   local menu_item_text="Stop all other Magento apps"
   has_no_args &&
     echo "$menu_item_text" && return
   [[ "$menu_item_text" != "$BASH_ARGV" ]] && return
-  echo_in_terminal "$BASH_ARGV"
+
+  # function logic
+  {
+    timestamp_msg "$menu_item_text"
+    docker-compose stop
+  } >> "$(get_log_file)" 2>&1
   exit
 }
 
 menu_items=(
+  update_this_management_app
   install_app
   open_app
   stop_app
-  resume_app
-  reset_app
+  restart_app
   sync_app_to_remote
   clone_app
-  start_shell
+  start_shell_in_app
+  start_management_shell
   show_app_logs
+  show_management_app_log
   uninstall_app
-  uninstall_other_apps
   stop_other_apps
 )
 
